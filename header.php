d2da4d86068a9029582bb426d538315b5cf84c3d
Courtney VanCaeyzeele <cvancaeyzeele@gmail.com>
2016-11-03 16:25:57 -0500

    Added profile page

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
                 <a class="navbar-brand" href="index.php">Pok&eacute;Lookup</a>
             </div>
             <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Home</a></li>
-                <li><a href="typechecker.php">Type Checker</a></li>
-                <li><a href="items.php">Items</a></li>
-                <li><a href="moves.php">Moves</a></li>
+                <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'index.php') { echo 'active'; } ?>"><a href="index.php">Home</a></li>
+                <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'typechecker.php') { echo 'active'; } ?>"><a href="typechecker.php">Type Checker</a></li>
+                <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'items.php') { echo 'active'; } ?>"><a href="items.php">Items</a></li>
+                <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'moves.php') { echo 'active'; } ?>"><a href="moves.php">Moves</a></li>
                 <?php if (isset($_SESSION['user'])): ?>
                     <li><a href="logout.php">Log Out</a></li>
                 <?php else: ?>
@@ -16,7 +16,7 @@
                 <?php endif; ?>
             </ul>
             <?php if (isset($_SESSION['loggedin'])): ?>
-                <p class="navbar-text navbar-right">Signed in as <?= $_SESSION['user'] ?></p>
+                <p class="navbar-text navbar-right"><a href="profile.php?id=<?= $_SESSION['user'] ?>">Signed in as <?= $_SESSION['user'] ?></a></p>
             <?php else: ?>
                 <p class="navbar-text navbar-right">Not logged in</p>
             <?php endif; ?>
---
+++ profile.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Pok&eacute;Lookup</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="css/main-styles.css" />
+        <link rel="stylesheet" type="text/css" href="css/profile-styles.css" />
+        <script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <? include 'header.php'; ?>
+        <div class="panel panel-default">
+
+        </div>
+    </body>
+</html>
