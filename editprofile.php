7a2ced548839d2f685f79c43d03901de5914195c
Courtney VanCaeyzeele <cvancaeyzeele@gmail.com>
2016-11-03 22:37:42 -0500

    Added editprofile.php so users can edit their profiles

---
+++ editprofile.php
@@ -0,0 +1,90 @@
+<?php
+    require 'connect.php';
+    session_start();
+
+    // make sure user is logged in
+    if (isset($_SESSION['loggedin'])) {
+        $query = "SELECT * FROM users WHERE username = :username";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $_SESSION['user']);
+        $statement->execute();
+        $user = $statement->fetch();
+    }
+
+    if (isset($_POST['submit'])) {
+        $realname = filter_input(INPUT_POST, 'realname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $bio = filter_input(INPUT_POST, 'bio', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        if (isset($realname) && isset($bio)) {
+            if (strlen($realname) < 1 && strlen($bio) > 0) { // if realname is empty update column to null
+                $update = "UPDATE users SET realname = NULL, bio = :bio WHERE username = :username";
+                $updateStatement = $db->prepare($update);
+                $updateStatement->bindValue(':username', $_SESSION['user']);
+                $updateStatement->bindValue(':bio', $bio);
+                $updateStatement->execute();
+            } elseif (strlen($bio) < 1 && strlen($realname) > 0) { // if bio is empty update column to null
+                $update = "UPDATE users SET realname = :realname, bio = NULL WHERE username = :username";
+                $updateStatement = $db->prepare($update);
+                $updateStatement->bindValue(':username', $_SESSION['user']);
+                $updateStatement->bindValue(':realname', $realname);
+                $updateStatement->execute();
+            } elseif (strlen($bio) < 1 && strlen($$realname) < 1) { // if both are empty ....
+                $update = "UPDATE users SET realname = NULL, bio = NULL WHERE username = :username";
+                $updateStatement = $db->prepare($update);
+                $updateStatement->bindValue(':username', $_SESSION['user']);
+                $updateStatement->execute();
+            } else {
+                $update = "UPDATE users SET realname = :realname, bio = :bio WHERE username = :username";
+                $updateStatement = $db->prepare($update);
+                $updateStatement->bindValue(':username', $_SESSION['user']);
+                $updateStatement->bindValue(':realname', $realname);
+                $updateStatement->bindValue(':bio', $bio);
+                $updateStatement->execute();
+            }
+
+            header('Location: profile.php');
+            exit();
+        }
+    }
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Pok&eacute;Lookup</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="css/main-styles.css" />
+        <link rel="stylesheet" type="text/css" href="css/profile-styles.css" />
+        <script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://use.fontawesome.com/f51889d3c4.js"></script>
+    </head>
+    <body>
+        <? include 'header.php'; ?>
+        <div class="panel panel-default">
+            <!-- if user is logged in -->
+            <?php if (isset($_SESSION['loggedin'])): ?>
+                <form method="post" action="" id="edit">
+                    <div class="form-group">
+                        <label for="realname">Name</label>
+                        <input type="text" class="form-control" id="realname" name="realname" placeholder="Full name" value="<?= $user['realname'] ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="bio">Bio</label>
+                        <textarea name="bio" id="bio" rows="5" maxlength="500" class="form-control" placeholder="Write something about yourself!"><?= $user['bio'] ?></textarea>
+                    </div>
+                    <input type="submit" name="submit" class="btn btn-default" value="Save Changes" />
+                </form>
+            <!-- warning message with link to login page if user not logged in -->
+            <?php else: ?>
+                <div class="alert alert-warning" role="alert">
+                    <p>Please <a href="login.php" class="alert-link">log in.</a></p>
+                </div>
+            <?php endif; ?>
+        </div>
+    </body>
+</html>
--- profile.php
+++ profile.php
@@ -1,5 +1,26 @@
 <?php
+    require 'connect.php';
     session_start();
+
+    // if user is logged in and there is no id in url, profile shown will be theirs
+    // if user is not logged in and there is no id in url, error and link to log in will be shown
+    // if there is an id in url, that profile will be shown regardless if user is logged in or not
+    if (isset($_SESSION['loggedin']) || isset($_GET['id'])) {
+        $username;
+
+        // make sure a username is included in page url
+        if (isset($_GET['id'])) {
+            $username = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        } elseif (isset($_SESSION['user'])) { // if no username passed through GET, get it from session variable
+            $username = $_SESSION['user'];    // in case user types in url manually and doesn't click link
+        } // no else; html code below displays error and link to login page if user has not logged in
+
+        $query = "SELECT * FROM users WHERE username = :username";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->execute();
+        $user = $statement->fetch();
+    }
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,11 +36,51 @@
             integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
             crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://use.fontawesome.com/f51889d3c4.js"></script>
     </head>
     <body>
         <? include 'header.php'; ?>
         <div class="panel panel-default">
+            <!-- if user is logged in or id is passed through GET -->
+            <?php if (isset($_SESSION['loggedin']) || isset($_GET['id'])): ?>
+                <h2><?= $user['username'] ?></h2>
+                <!-- display real name if set -->
+                <?php if (!is_null($user['realname'])): ?>
+                    <h2 class="realname"><small><?= $user['realname'] ?></small></h2>
+                <?php endif; ?>
 
+                <!-- if user is on their profile and logged in -->
+                <?php if (isset($_SESSION['loggedin'])): ?>
+                    <?php if ($_SESSION['user'] === $user['username']): ?>
+                        <a href="editprofile.php" type="button" class="btn btn-default edit-btn" role="button">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                    <?php endif; ?>
+                <?php endif; ?>
+
+                <!-- display profile picture if set, placeholder if not -->
+                <?php if (is_null($user['picture'])): ?>
+                    <img src="https://api.adorable.io/avatars/80/<?= $user['userid'] ?>.png" alt="profile-picture" class="img-rounded" />
+                <?php else: ?>
+                    <img src="<?= 'data:image/jpeg;base64,'.base64_encode( $user['picture'] ) ?>" alt="profile-picture" class="img-rounded" />
+                <?php endif; ?>
+
+                <!-- display bio if set, placeholder text if not -->
+                <?php if (is_null($user['bio'])): ?>
+                    <p class="lead">No bio available</p>
+                <?php else: ?>
+                    <p class="lead"><?= $user['bio'] ?></p>
+                <?php endif; ?>
+
+                <!-- buttons to display posts or comments -->
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default">Posts</button>
+                    <button type="button" class="btn btn-default">Comments</button>
+                </div>
+            <!-- warning message with link to login page if user not logged in and no id passed -->
+            <?php else: ?>
+                <div class="alert alert-warning" role="alert">
+                    <p>Please <a href="login.php" class="alert-link">log in.</a></p>
+                </div>
+            <?php endif; ?>
         </div>
     </body>
 </html>
